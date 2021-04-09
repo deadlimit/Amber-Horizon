4570a2c523d7363bb4f187c756f8664806eb627b
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2021-04-09 12:20:03 +0200

    CameraFix

--- SPM/Assets/Camera/ThirdPersonCamera.cs
+++ SPM/Assets/Camera/ThirdPersonCamera.cs
@@ -29,7 +29,8 @@ public class ThirdPersonCamera : MonoBehaviour
     {     
         GetInput();
         //rotationY =  Mathf.Clamp(rotationY, -90, 90);
-        rotationX = Mathf.Clamp(rotationX, -80, 80);    
+        rotationX = Mathf.Clamp(rotationX, -80, 80);
+        offset = transform.rotation * cameraOffset;
         PlaceCamera();
         transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
     }
@@ -42,17 +43,16 @@ public class ThirdPersonCamera : MonoBehaviour
     void PlaceCamera() {     
         RaycastHit hitInfo;
         playerPos = player.transform.position;
-        offset = transform.rotation * cameraOffset;
+
     
-        if (Physics.SphereCast(playerPos, coll.radius, offset.normalized, out hitInfo, cameraOffset.magnitude, collisionMask))
-        {
-            //hitInfo transform är collidern vi krockar med. lite tokigt? 
-            Vector3 hitOffset = new Vector3(cameraOffset.x, hitInfo.point.y + coll.radius / 2, hitInfo.point.z + coll.radius / 2);
-            offset = transform.rotation * (hitOffset.normalized * hitInfo.distance);
+        if (Physics.SphereCast(playerPos, coll.radius, offset.normalized, out hitInfo, offset.magnitude, collisionMask))
+        { 
+            Vector3 hitOffset = new Vector3(cameraOffset.x, hitInfo.point.y - coll.radius/2, hitInfo.point.z - coll.radius/2);
+            offset = hitInfo.distance * offset.normalized;;
         }
         Debug.DrawLine(playerPos, playerPos + offset  , Color.green);
 
-        Vector3 lerp = Vector3.Lerp(transform.position, offset + playerPos, camSpeed * Time.deltaTime);
-        transform.position = lerp;
+        transform.position = Vector3.Lerp(transform.position, playerPos + offset, camSpeed * Time.deltaTime);
+
     }
 }
