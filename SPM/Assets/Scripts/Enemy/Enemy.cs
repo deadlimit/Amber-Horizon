22d774a53ea9514b8b9950a4a73240ade6a80cea
SOTGGON <maoxiehui@outlook.com>
2021-04-16 18:24:05 +0200

    Revert "Merge branch 'Level1_Whitebox' of https://github.com/deadlimit/SPM_Grupp12 into Level1_Whitebox"
    This reverts commit 16309846b86ab1a4627ca2cd21e98157af360e5f, reversing
    changes made to 9c614b6882b8ae9ec5e9c6e471b460745cfd2d92.

--- SPM/Assets/Scripts/Enemy/Enemy.cs
+++ SPM/Assets/Scripts/Enemy/Enemy.cs
@@ -2,42 +2,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public abstract class Enemy : MonoBehaviour, IBlackHoleBehaviour
-{
+public abstract class Enemy : MonoBehaviour {
 
-    public float outerRing, innerRing;
-    public PhysicsComponent PhysicsComponent { get; set; }
-    public Animator Animator { get; private set; }
-    public AIPathfinder Pathfinder { get; private set; }
-    public Transform Target { get; private set; }
-    public CapsuleCollider Collider { get; private set; }
-
-    public LayerMask PlayerMask;
-
-    [SerializeField] private State[] states;
-
+    protected PhysicsComponent physicsComponent;
+    protected Animator animator;
+    protected AIPathfinder pathfinder;
+    
+    [SerializeField] protected State[] states;
+    
     public StateMachine stateMachine { get; private set; }
-
-    protected void Awake()
-    {
+    
+    private void Awake() {
         stateMachine = new StateMachine(this, states);
-        Animator = GetComponent<Animator>();
-        PhysicsComponent = GetComponent<PhysicsComponent>();
-        Collider = GetComponent<CapsuleCollider>();
-        Pathfinder = GetComponent<AIPathfinder>();
-        Target = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+        physicsComponent = GetComponent<PhysicsComponent>();
+        pathfinder = GetComponent<AIPathfinder>();
     }
 
-    protected void Update()
-    {
-        Animator.SetFloat("DistanceToTarget", Vector3.Distance(transform.position, Pathfinder.agent.destination));
-    }
-
-    public bool ProximityCast(float radius)
-    {
-        return Physics.OverlapSphere(transform.position, radius, PlayerMask).Length > 0;
-    }
-
-
-    public virtual void BlackHoleBehaviour(BlackHole blackHole) { Debug.Log("hello"); }
+    public abstract void BlackHoleDeath(BlackHole blackHole);
 }
