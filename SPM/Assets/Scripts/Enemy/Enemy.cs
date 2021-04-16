d566f249ec3f62e30e61888a6fb694747da4ed7e
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2021-04-16 17:59:21 +0200

    Trasigt inheritance

--- SPM/Assets/Scripts/Enemy/Enemy.cs
+++ SPM/Assets/Scripts/Enemy/Enemy.cs
@@ -2,22 +2,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public abstract class Enemy : MonoBehaviour {
+public abstract class Enemy : MonoBehaviour, IBlackHoleBehaviour
+{
 
-    protected PhysicsComponent physicsComponent;
-    protected Animator animator;
-    protected AIPathfinder pathfinder;
-    
-    [SerializeField] protected State[] states;
-    
+    public float outerRing, innerRing;
+    public PhysicsComponent PhysicsComponent { get; set; }
+    public Animator Animator { get; private set; }
+    public AIPathfinder Pathfinder { get; private set; }
+    public Transform Target { get; private set; }
+    public CapsuleCollider Collider { get; private set; }
+
+    public LayerMask PlayerMask;
+
+    [SerializeField] private State[] states;
+
     public StateMachine stateMachine { get; private set; }
-    
-    private void Awake() {
+
+    protected void Awake()
+    {
         stateMachine = new StateMachine(this, states);
-        animator = GetComponent<Animator>();
-        physicsComponent = GetComponent<PhysicsComponent>();
-        pathfinder = GetComponent<AIPathfinder>();
+        Animator = GetComponent<Animator>();
+        PhysicsComponent = GetComponent<PhysicsComponent>();
+        Collider = GetComponent<CapsuleCollider>();
+        Pathfinder = GetComponent<AIPathfinder>();
+        Target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    public abstract void BlackHoleDeath(BlackHole blackHole);
+    protected void Update()
+    {
+        Animator.SetFloat("DistanceToTarget", Vector3.Distance(transform.position, Pathfinder.agent.destination));
+    }
+
+    public bool ProximityCast(float radius)
+    {
+        return Physics.OverlapSphere(transform.position, radius, PlayerMask).Length > 0;
+    }
+
+
+    public virtual void BlackHoleBehaviour(BlackHole blackHole) { Debug.Log("hello"); }
 }
