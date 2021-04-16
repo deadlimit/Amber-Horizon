c52b3860b1064aaf0c9eb0dce7c09bff09a5dd06
mmmart-in <63650287+mmmart-in@users.noreply.github.com>
2021-04-16 11:41:34 +0200

    KeyFragPrefab

--- SPM/Assets/Scripts/GameVariables.cs
+++ SPM/Assets/Scripts/GameVariables.cs
@@ -15,5 +15,6 @@ public class GameVariables : ScriptableObject
         //ingen aning om den wipas vid rätt tillfälle här, vill ha persistent data men egentligen inte för just denna variabel
         keyFragments = 0;
     }
-    public bool RequiredNoOfKeys() {return ++keyFragments >= requiredKeyNumber;}
+    public bool RequiredNoOfKeys() { return ++keyFragments >= requiredKeyNumber; }
+    public void EnableDash() { dashAvailable = true; }
 }
