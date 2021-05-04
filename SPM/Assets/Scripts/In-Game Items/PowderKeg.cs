83b8c69e31767ba338c6550ca752bc607bad3662
Hampus424 <64363889+Hampus424@users.noreply.github.com>
2021-05-04 16:51:15 +0200

    Fixed explosion vfx on PowderKegs
    Small change script Powder Keg + Keg prefab

--- SPM/Assets/Scripts/In-Game Items/PowderKeg.cs
+++ SPM/Assets/Scripts/In-Game Items/PowderKeg.cs
@@ -6,12 +6,14 @@ using EventCallbacks;
 public class PowderKeg : MonoBehaviour
 {
     public LayerMask collisionMask;
+    public GameObject explosion;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 8)
         {
             EventSystem<ExplosionEvent>.FireEvent(new ExplosionEvent(transform.position));
+            Instantiate(explosion, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
