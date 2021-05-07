38c0cb58a9fc1b7a30a1ad4f4b6bdf1ec8a3bf0c
Hampus424 <64363889+Hampus424@users.noreply.github.com>
2021-05-07 17:11:13 +0200

    Some more small changes to UI

--- SPM/Assets/MenuTest.cs
+++ SPM/Assets/MenuTest.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuTest : MonoBehaviour
 {
     [SerializeField] private GameObject menu;
+    public string MainMenuSceneName;
 
     void Start()
     {
@@ -62,6 +64,16 @@ public class MenuTest : MonoBehaviour
         GameObject camObj = GameObject.FindWithTag("MainCamera");
         camObj.GetComponent<ThirdPersonCamera>().enabled = true;
 
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        print("load main menu scene");
     }
 
 }
