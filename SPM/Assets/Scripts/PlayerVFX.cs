a467bf146e1254d218a0597017054364886ab4e6
Hampus424 <64363889+Hampus424@users.noreply.github.com>
2021-05-21 17:53:17 +0200

    Implemented-ish effect on pickup

--- SPM/Assets/Scripts/PlayerVFX.cs
+++ SPM/Assets/Scripts/PlayerVFX.cs
@@ -7,9 +7,18 @@ public class PlayerVFX : MonoBehaviour
     public GameObject position;
     public GameObject DashEffect;
 
+    public GameObject RingEffect;
 
     public void PlayDashStart()
     {
         Instantiate(DashEffect, position.transform.position, position.transform.rotation, this.gameObject.transform);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Pickup"))
+            return;
+
+        Instantiate(RingEffect, this.transform.position, this.transform.rotation, this.gameObject.transform);
     }
 }
