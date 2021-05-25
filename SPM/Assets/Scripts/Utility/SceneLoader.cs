fcc118420fc4cf84c07fc36d5180803456e00ff1
deadlimit <47717668+deadlimit@users.noreply.github.com>
2021-05-25 10:58:15 +0200

    New load scenes

--- SPM/Assets/Scripts/Utility/SceneLoader.cs
+++ SPM/Assets/Scripts/Utility/SceneLoader.cs
@@ -5,30 +5,60 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
-    
+
+    private static Scene previousActiveScene;
+   
     [MenuItem("Load scenes/Load Main Menu")]
     private static void LoadMainMenu() {
         UnloadCurrentScenes();
-        LoadScene("MainMenu");
+        SetNewActiveScene(LoadScene("MainMenu"));
         LoadScene("ProjectileScene");
+        UnloadPreviousActiveScene();
     }
    
     [MenuItem("Load scenes/Load Level 1")]
     private static void LoadLevel1() {
         UnloadCurrentScenes();
-        LoadScene("BaseScene");
+        SetNewActiveScene(LoadScene("BaseScene"));
         LoadScene("Level 1");
         LoadScene("ProjectileScene");
+        UnloadPreviousActiveScene();
     }
 
-    private static void LoadScene(string sceneName) {
-        EditorSceneManager.OpenScene("Assets/Scenes/ActiveScenes/" + sceneName + ".unity", OpenSceneMode.Additive);
+    [MenuItem("Load scenes/Load Level 2")]
+    private static void LoadLevel2() {
+        UnloadCurrentScenes();
+        SetNewActiveScene(LoadScene("BaseScene"));
+        LoadScene("Level 2");
+        LoadScene("ProjectileScene");
+        UnloadPreviousActiveScene();
+    }
+
+    
+    private static Scene LoadScene(string sceneName) {
+        return EditorSceneManager.OpenScene("Assets/Scenes/ActiveScenes/" + sceneName + ".unity", OpenSceneMode.Additive);
     }
 
     private static void UnloadCurrentScenes() {
-        for (int i = 0; i < EditorSceneManager.loadedSceneCount; i++)
-            EditorSceneManager.CloseScene(EditorSceneManager.GetSceneAt(i), true);
+        previousActiveScene = EditorSceneManager.GetActiveScene();
+        
+        for (int i = 0; i < EditorSceneManager.loadedSceneCount; i++) {
+            
+            Scene currentScene = EditorSceneManager.GetSceneAt(i);
+            
+            if(currentScene == previousActiveScene)
+                continue;
+            
+            EditorSceneManager.CloseScene(currentScene, true);
+        }
+
     }
 
+    private static void UnloadPreviousActiveScene() {
+        EditorSceneManager.CloseScene(previousActiveScene, true);
+    }
+    private static void SetNewActiveScene(Scene scene) {
+        EditorSceneManager.SetActiveScene(scene);
+    }
 
 }
