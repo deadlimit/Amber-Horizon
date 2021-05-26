93311c46b36a10f0e5ccdeffd5982c2a902fd776
deadlimit <47717668+deadlimit@users.noreply.github.com>
2021-05-26 13:29:10 +0200

    Auto detect resolution

--- SPM/Assets/SettingsController.cs
+++ SPM/Assets/SettingsController.cs
@@ -1,22 +1,26 @@
 using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour {
 
-    [SerializeField] private Settings settings;
+    [SerializeField] private Settings.Settings settings;
     
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private TMP_Dropdown resolutionList;
+    [SerializeField] private TMP_Dropdown displayModeList;
 
     private string[] chosenResolution;
         
     private void Awake() {
         volumeSlider.onValueChanged.AddListener(UpdateMaserVolume);
         resolutionList.onValueChanged.AddListener(ChangeResolution);
+        displayModeList.onValueChanged.AddListener(ChangeDisplayMode);
         chosenResolution = new string[2];
+        
+        AutoDetectScreenResolution();
+        
     }
     
     private void UpdateMaserVolume(float newValue) {
@@ -24,17 +28,41 @@ public class SettingsController : MonoBehaviour {
     }
 
     private void ChangeResolution(int index) {
-        chosenResolution = resolutionList.options[index].text.Split('x');
 
+        if (!resolutionList.options[index].text.Contains("x")) {
+            AutoDetectScreenResolution();
+            return;
+        }
+        
+        chosenResolution = resolutionList.options[index].text.Split('x');
+        
         int width = Int32.Parse(chosenResolution[0]);
         int height = Int32.Parse(chosenResolution[1]);
 
-        Resolution resolution = new Resolution();
+        Settings.Resolution resolution;
 
-        resolution.width = width;
-        resolution.height = height;
+        resolution.Width = width;
+        resolution.Height = height;
 
         settings.Resolution = resolution;
+    }
+
+    private void ChangeDisplayMode(int index) {
+        string choice = displayModeList.options[index].text;
+        
+        switch (choice) {
+            case "Fullscreen":
+                Screen.fullScreen = true;
+                break;
+            case "Windowed":
+                Screen.fullScreen = false;
+                break;
+            
+        }
+    }
+
+    public void AutoDetectScreenResolution() {
+        resolutionList.value = resolutionList.options.FindIndex(resolutionOption => resolutionOption.text == $"{Screen.currentResolution.width}x{Screen.currentResolution.height}");
     }
     
 }
