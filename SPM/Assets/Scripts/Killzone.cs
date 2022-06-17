09f50387b53e07c064b984552c4de33a50638aa1
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2022-06-17 13:43:13 +0200

    Finished start area.

--- SPM/Assets/Scripts/Killzone.cs
+++ SPM/Assets/Scripts/Killzone.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using EventCallbacks;
+using System.Collections.Generic;
+using System.Collections;
 
 public class Killzone : MonoBehaviour {
         
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) 
         {
-            PlayerDiedEvent pde = new PlayerDiedEvent(other.gameObject);
-            EventSystem<PlayerDiedEvent>.FireEvent(pde);
+            StartCoroutine(WaitBeforeDeathEvent(other));
         }           
     }
+
+    private IEnumerator WaitBeforeDeathEvent(Collider other)
+    {
+
+       yield return new WaitForSeconds(.15f);
+
+       PlayerDiedEvent pde = new PlayerDiedEvent(other.gameObject);
+       EventSystem<PlayerDiedEvent>.FireEvent(pde);
+    }
+
 }
