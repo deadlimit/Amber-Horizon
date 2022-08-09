969e29c3406958b1d0d9d3e045984dff6fdef681
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2022-08-09 16:01:44 +0200

    fixed the back button in settings.
    also animations for fading in and out.

--- SPM/Assets/Menu/MenuController.cs
+++ SPM/Assets/Menu/MenuController.cs
@@ -24,12 +24,18 @@ public class MenuController : MonoBehaviour {
 
     private IEnumerator LoadLevels() {
 
+        //CAS should be the main menu scene.
         Scene currentActiveScene = SceneManager.GetActiveScene();
-        if (nameOfLevelToLoad == "Intro Cutscene") 
+        if (nameOfLevelToLoad == "Intro Cutscene")
         {
-            Debug.Log("In MC, LoadLevels. Hypothetical spot for loading intro cutscene.");
-        }
+            
 
+            Debug.Log("In MC, LoadLevels. Hypothetical spot for loading intro cutscene.");
+            yield return SceneManager.LoadSceneAsync("Intro_Cutscene", LoadSceneMode.Additive);
+
+            yield return SceneManager.UnloadSceneAsync(currentActiveScene);
+        }
+        else { 
         //then check nameOfLevelToLoad and load the appropriate scene. 
         yield return SceneManager.LoadSceneAsync("Level 1 V2", LoadSceneMode.Additive);
 
@@ -37,14 +43,16 @@ public class MenuController : MonoBehaviour {
         
         yield return SceneManager.LoadSceneAsync("ProjectileScene", LoadSceneMode.Additive);
 
+        
         //I think the level that is loaded should be the active scene. But who knows.
         Scene newActiveScene = SceneManager.GetSceneByName("BaseScene");
+
 
         SceneManager.SetActiveScene(newActiveScene);
         EventSystem<NewLevelLoadedEvent>.FireEvent(null);
         EventSystem<ActivatePlayerControl>.FireEvent(new ActivatePlayerControl(true));
         yield return SceneManager.UnloadSceneAsync(currentActiveScene);
-        
+        }
 
     }
     
