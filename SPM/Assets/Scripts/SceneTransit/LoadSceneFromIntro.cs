04fb052d2e78d7ab658226916be925a720122bc2
AngryBread-git <67788270+AngryBread-git@users.noreply.github.com>
2022-08-18 12:34:00 +0200

    Dolly mostly done.
    Now for the scene skipping logic.

--- SPM/Assets/Scripts/SceneTransit/LoadSceneFromIntro.cs
+++ SPM/Assets/Scripts/SceneTransit/LoadSceneFromIntro.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using EventCallbacks;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LoadSceneFromIntro : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI textInUI;
+    private bool firstInputMade = false;
+
+    //I don't want the player loading the next level at the same time as the game starts the load.
+    //Could cause delay.
+    //This value is changed by a signal in the intro cutscene.
+    public bool lateInIntro = false;
+
+    private void Start()
+    {
+        textInUI.text = "Press Esc\nto skip.";
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) 
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Debug.Log("Pressed ESC in intro.");
+            FirstInput();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !lateInIntro)
+        {
+            Debug.Log("Pressed space in intro.");
             //StartCoroutine(LoadLevel1());
         }
     }
@@ -30,12 +51,16 @@ public class LoadSceneFromIntro : MonoBehaviour
 
         yield return SceneManager.LoadSceneAsync("ProjectileScene", LoadSceneMode.Additive);
 
-
-
         EventSystem<NewLevelLoadedEvent>.FireEvent(null);
         EventSystem<ActivatePlayerControl>.FireEvent(new ActivatePlayerControl(true));
 
         //I think this has to happen last. So it doesn't unload the scene while the script is still running.
         yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+    }
+
+    private void FirstInput() 
+    {
+        textInUI.text = "Press Space\nto confirm.";
+        firstInputMade = true;
     }
 }
